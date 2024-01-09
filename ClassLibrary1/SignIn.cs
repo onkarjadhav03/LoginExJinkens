7aa967c3d96c356046a3365bc20ff615c3a75ba5
onkar <jadhavonkar333@gmail.com>
2024-01-09 12:41:48 +0530

    first commit

---
+++ ClassLibrary1/SignIn.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class SignIn
+    {
+        public static string Authenticate(string username, string password)
+        {
+            string result;
+            if(string.IsNullOrEmpty(username) || string.IsNullOrEmpty(password))
+            {
+                result = "you must provide username and password";
+            }
+            else if(username == "sam" || password == "sam@123")
+            {
+                result = "valid user";
+            }
+            else
+            {
+                result = "invalid user";
+            }
+
+            return result;
+        }
+    }
+}
---
+++ TestProject1/SingleTest.cs
@@ -0,0 +1,35 @@
+﻿using ClassLibrary1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject1
+{
+    [TestClass]
+    public class SingleTest
+    {
+        [TestMethod]
+
+        public void SuccessMethod()
+        {
+            string expResult = "valid user";
+            Assert.AreEqual(expResult, SignIn.Authenticate("sam", "sam@123"));
+        }
+
+        [TestMethod]
+
+        public void FailureMethod()
+        {
+            string expResult = "invalid user";
+            Assert.AreEqual(expResult, SignIn.Authenticate("raj", "raj@123"));
+        }
+        [TestMethod]
+        public void AnonymousMethod()
+        {
+            string expResult = "you must provide username and password";
+            Assert.AreEqual(expResult, SignIn.Authenticate(null, null));
+        }
+    }
+}
